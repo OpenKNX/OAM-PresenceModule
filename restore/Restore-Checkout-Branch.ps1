a01c2abbe60670f44140f01b3a6bbdad78b7cdca
Waldemar Porscha <wp@porscha.eu>
2022-11-30 10:48:17 +0100

    Restore - next try

--- restore/Restore-Checkout-Branch.ps1
+++ restore/Restore-Checkout-Branch.ps1
@@ -1,7 +1,12 @@
 $subprojects = Get-Content dependencies.txt
+$currentDir = Get-Location
 foreach ($subproject in $subprojects) {
     $attr = $subproject.Split()
     if ($attr[0] -ne "-------") {
-        git --git-dir "$($attr[2])/.git" checkout $attr[1]
+        # the following does not work as expected
+        # git --git-dir "$($attr[2])/.git" checkout $attr[1]
+        Set-Location $attr[2]
+        git checkout $attr[1]
+        Set-Location $currentDir
     }
 }
