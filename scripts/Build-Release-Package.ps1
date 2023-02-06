af3f9a5444f6a129f6112c51a53ba4b2a9fc506e
Waldemar Porscha <wp@porscha.eu>
2023-02-06 12:57:54 +0100

    Release preparation 1.7.2
    - introduced Release-Package script
    - supports new RealPresence HW 2.0

---
+++ scripts/Build-Release-Package.ps1
@@ -0,0 +1,29 @@
+# This script should be called from .vscode/tasks.json with
+# {
+#     "label": "Build-Release-Package",
+#     "type": "shell",
+#     "command": "scripts/Build-Release-Package.ps1",
+#     "args": [
+#     ],
+#     "problemMatcher": [],
+#     "group": "test"
+# },
+
+# check and cleanup working dir
+if (Test-Path -Path release-package) {
+    # clean working dir
+    Remove-Item -Recurse release-package\*
+} else {
+    New-Item -Path release-package -ItemType Directory | Out-Null
+}
+
+# list here all release variants you want to release as a package
+scripts/Build-Release.ps1 Big
+if (!$?) { exit 1 }
+Copy-Item release/* release-package
+
+scripts/Build-Release.ps1 Release
+if (!$?) { exit 1 }
+Copy-Item release/* release-package
+
+Write-Host "Release package sucessfully created!"
