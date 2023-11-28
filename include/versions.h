b14341fc1fc63ca2e0f131a7df2ffec2d8b3178b
Waldemar Porscha <wp@porscha.eu>
2023-11-28 20:03:48 +0100

    Change to new producers generate

--- include/versions.h
+++ include/versions.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#define MAIN_Version "5a793ea"
-#define KNX_Version "4c16a38"
-#define MODULE_LogicModule_Version "2.0.3+ddc9cfc"
+#define MAIN_Version "58c093a"
+#define KNX_Version "e602b59"
+#define MODULE_LogicModule_Version "2.0.3+6212270"
 #define MODULE_LogicModule_ETS 32
-#define MODULE_Common_Version "1.0.0+3ed3815"
+#define MODULE_Common_Version "1.0.9+b4610cb"
 #define MODULE_Common_ETS 16
-#define MODULE_PresenceModule_Version "2.0.0+3b98b12"
+#define MODULE_PresenceModule_Version "2.0.0+b9095dd"
 #define MODULE_PresenceModule_ETS 32
-#define MODULE_SensorDevices_Version "1.0.0+536155c"
+#define MODULE_SensorDevices_Version "1.0.0+c56ec2e"
 #define MODULE_SensorDevices_ETS 16
 #define MODULE_SmartMF_Version "05bdb51"
+#define MODULE_Updater_Version "0.0.0+0fa5d39"
+#define MODULE_Updater_ETS 0
